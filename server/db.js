ea882d4474ffe3ccb8dd34dec754309f1f2777de
mrxk <mrxk@mrxk>
2023-12-04 14:23:03 -0500

    feat: support max connections

--- server/db.js
+++ server/db.js
@@ -1,3 +1,8 @@
 import postgres from 'postgres'
-const sql = postgres({ssl: 'prefer'})
+
+var max = 8080
+if (process.env.MAX_CONNECTIONS !== undefined) {
+    max = parseInt(process.env.MAX_CONNECTIONS, 10)
+}
+const sql = postgres({ssl: 'prefer', max: max})
 export default sql
